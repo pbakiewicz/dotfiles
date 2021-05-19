e003dbf4bb2cc7c4ff045595614ccd552652c023
Pawel <bakiewicz.p@gmail.com>
2021-05-19 19:54:55 +0200

    Delete unneccesery abbrvs. All of them are replaced with simple ff.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,11 +5,8 @@ set -gx PATH /home/pawel/bin/ $PATH
 set -gx PATH /home/pawel/bin/ssh/ $PATH
 set -gx PATH /home/pawel/.npm-global/bin/ $PATH
 # {{{ abbrs
-abbr vim nvim
+abbr v nvim
 abbr n newsboat
-abbr v "rg --files --no-messages  --hidden $HOME/dotfiles/ -g '!.git'| fzf | xargs -r nvim"
-abbr vi "fzf | xargs -r nvim"
-abbr va "rg --files --no-messages -g '!sys' -g '!proc' -g '!boot' / | fzf | xargs -r nvim"
 abbr .. "cd .."
 abbr ... "cd ../.."
 # }}}
