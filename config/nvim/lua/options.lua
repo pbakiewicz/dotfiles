8363a1f0759b230e655f55cbece25f7a690829e0
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-04-10 18:30:08 +0200

    First try at nvim lua config

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+local options = {
+    clipboard = "unnamedplus",
+    splitbelow = true,
+    splitright = true,
+    mouse ="a",
+    number = true,
+    relativenumber = true,
+    showtabline=2,
+    background = "dark",
+    foldmethod = "marker",
+    scrolloff = 8,
+    backup = false,
+    cmdheight = 2,
+    ignorecase = true, -- ignore case on search
+    smartcase = true, -- when type capital letter ignorecase is off
+    swapfile = false,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.cmd "set softtabstop=4 shiftwidth=4 noexpandtab"    
+
+
