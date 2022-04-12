9dcdf1b59d17c84e6cc9999942e8aabb5ee7a899
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-04-12 20:25:40 +0200

    Modify keymaps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -24,14 +24,16 @@ k("n", "gj", ":tabprev<CR>", opts)
 k("i", "jk", "<Esc>", opts)
 
 -- resizing
-k("n", "<M-j>", ":resize -2<CR>", opts)
-k("n", "<M-k>", ":resize +2<CR>", opts)
-k("n", "<M-h>", ":vertical resize -2<CR>", opts)
-k("n", "<M-l>", ":vertical resize +2<CR>", opts)
+k("n", "<C-Down>", ":resize -2<CR>", opts)
+k("n", "<C-Up>", ":resize +2<CR>", opts)
+k("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+k("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
-k("v", "<A-j>", ":m .+1<CR>==", opts)
-k("v", "<A-k>", ":m .-2<CR>==", opts)
-k("v", "p", '"_dP', opts)
 -- Move text up and down
+k("v", "<A-j>", ":m '>+1<CR>gv", opts)
+k("v", "<A-k>", ":m '<-2<CR>gv", opts)
+
+-- Pasting puts new value into void registry
+k("v", "p", '"_dP', opts)
 
 -- nmap <leader>g :FloatermNew --height=1.0 --width=1.0 --autoclose=1 lazygit<CR>
