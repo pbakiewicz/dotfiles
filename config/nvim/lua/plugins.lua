c12ea632c9833ae3b402232c186fd28bd6a8dd37
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-04-12 20:36:36 +0200

    Add most import plugins.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,8 @@
-return require('packer').startup(function(use)
+local packer = require('packer')
+
+return packer.startup(function(use)
     use 'wbthomason/packer.nvim'
+    use 'mhinz/vim-startify'
+    use 'francoiscabrol/ranger.vim'
+    use 'voldikss/vim-floaterm'
 end)
