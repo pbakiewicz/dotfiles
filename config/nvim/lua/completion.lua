4c24bcf6e777f8ea39149cdf5fd0592bcdbe1914
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-06-13 18:38:38 +0200

    Docs completion is not needed for me.
    Pressing K gives docs popup and pressing K again goes into
    
    popup. That is enought for docs. No need for special scrolling.

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -6,13 +6,6 @@ cmp.setup {
     mapping = {
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-j>"] = cmp.mapping.select_next_item(),
-      -- prawdopodobnie do docsow, powroc przy LSP
-      -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      -- ["<C-y>"] = cmp.mapping.confirm {
-      -- behavior = cmp.ConfirmBehavior.Insert,
-      -- select = True
-      -- },
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = true })
