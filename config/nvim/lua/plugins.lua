7271741aa9085f1f21816d574c23d0a8b2117660
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-06-14 20:10:14 +0200

    [Nvim] Reorder plugins.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,36 +1,34 @@
 local packer = require('packer')
 
 return packer.startup(function(use)
+    -- basic packages
     use 'wbthomason/packer.nvim'
-    use 'neovim/nvim-lspconfig' -- pretty much required for LSP to function
+    use 'neovim/nvim-lspconfig' -- LSP even tho builtin requires this package.
     use 'mhinz/vim-startify'
     use 'voldikss/vim-floaterm'
     use 'justinmk/vim-sneak'
+    use "f-person/git-blame.nvim" -- for blaming
+    -- colorschemes
     use 'lunarvim/colorschemes'
     use 'folke/tokyonight.nvim'
-
     -- telescope
     use "nvim-lua/plenary.nvim"
     use "nvim-telescope/telescope.nvim"
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use { "nvim-telescope/telescope-file-browser.nvim" }
-
-
-    -- completions
-    use "hrsh7th/nvim-cmp" -- core
+    -- completions core plugin
+    use "hrsh7th/nvim-cmp"
+    -- completions sources
     use "hrsh7th/cmp-nvim-lsp" -- LSP baby
     use "hrsh7th/cmp-buffer" -- buffer
     use "hrsh7th/cmp-path" -- path
     use "hrsh7th/cmp-nvim-lua" -- neovim Lua API
-
-    use "f-person/git-blame.nvim" -- for blaming
-
-    use "vim-test/vim-test" -- for unittests
-
+    use "saadparwaiz1/cmp_luasnip" -- snippets completion for LuaSnip specifically
     -- snippets
     -- https://youtu.be/_DnmphIwnjo?t=785
-    -- use "saadparwaiz1/cmp_luasnip"
-    -- use "L3MON4D3/LuaSnip"
+    use "L3MON4D3/LuaSnip"
     -- use "rafamadriz/friendly-snippets"
+    -- unit tests
+    use "vim-test/vim-test"
 
 end)
