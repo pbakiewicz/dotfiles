5366717efed5c7c264829d5ea4e0a49d645e91c4
Pawel Bakiewicz <pbakiewicz@icloud.com>
2024-03-19 17:24:16 +0100

    add git repo to telescope cwd.

--- config/nvim/lua/telescope-config.lua
+++ config/nvim/lua/telescope-config.lua
@@ -5,7 +5,17 @@ require("telescope").setup({
 
 })
 
-local glob_pattern = "*.py"
+-- in git repo always use live grep as a top path
+-- if not, use live grep in folder you are currently in.
+local git_top_level = vim.fn.systemlist("git rev-parse --show-toplevel")
+if vim.v.shell_error == 0 then
+    cwd = git_top_level[1]
+end
+
+-- to do set ff outsideof git repo to use file find
+
+local glob_pattern = "*.{py,go,lua}"
+
 
 local mappings = {
     live_grep = function()
