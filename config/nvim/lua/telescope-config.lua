96e73be4e14c4e4a313e2a57f5414b360bb45785
pbakiewicz <pbakiewicz@icloud.com>
2024-04-13 20:39:25 +0200

    Nvim remake (#1)
    * Firs try at options.
    
    * Change plugings.
    
    * Delete for now options.
    
    * add floatterm.
    
    * add startify.
    
    * add telescope.
    
    * Add treesitter.
    
    * tmux - pousuwanie starych opcji, dodanie captucchina.
    
    * ostatnie poprawki do tmuxa.
    
    * Change mode for bin file.
    
    * add git repo to telescope cwd.
    
    * Test
    
    * Test 2

--- config/nvim/lua/telescope-config.lua
+++ config/nvim/lua/telescope-config.lua
@@ -1,28 +1,25 @@
-require("telescope").setup{
+require("telescope").setup({
     defaults = {
-	prompt_prefix = "🦖: ",
+        prompt_prefix = " 🦖 > ",
     },
-    extensions = {
-	file_browser = {
-	    theme = "ivy",
-	    hijack_netrw = true,
-	}
-    }
-}
 
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("file_browser")
+})
 
+-- in git repo always use live grep as a top path
+-- if not, use live grep in folder you are currently in.
+local git_top_level = vim.fn.systemlist("git rev-parse --show-toplevel")
+if vim.v.shell_error == 0 then
+    cwd = git_top_level[1]
+end
 
-local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-local glob_pattern = "*.py"
+-- to do set ff outsideof git repo to use file find
+
+local glob_pattern = "*.{py,go,lua}"
+
 
 local mappings = {
     live_grep = function()
 	require("telescope.builtin").live_grep({cwd=cwd, glob_pattern=glob_pattern})
-    end,
-    file_browser = function()
-	require ("telescope").extensions.file_browser.file_browser({cwd=cwd})
     end,
 }
 
