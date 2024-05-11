6adff756ae4af0b728ea0ff8407178169900c343
Paweł Bąkiewicz <pbakiewicz@icloud.com>
2024-05-11 15:11:59 +0200

    [i3] set_resolution.
    Dodanie jeszcze jednego trybu.

--- bin/set_resolution.sh
+++ bin/set_resolution.sh
@@ -7,7 +7,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
 # Check if DP-1 is connected
-if xrandr | grep "DP-1 connected" | grep -v "eDP-1"; then
+if xrandr | grep "HDMI-1-0 connected"; then
+	xrandr --output eDP-1 --off
+    xrandr --output HDMI-1-0 --mode 3440x1440 --rate 99.90
+	polybar krzywy &
+elif xrandr | grep "DP-1 connected" | grep -v "eDP-1"; then
 	xrandr --output eDP-1 --off
 	polybar krzywy &
 elif xrandr | grep "DP-4 connected"; then
@@ -20,3 +24,4 @@ fi
 
 # Ustawianie wallpapera bo inaczej jest krzywy.
 feh --bg-center "$(find $HOME/o/wallpapers/3440x1440/ -type f | shuf -n1)"
+
