bfa8d1211831bf5126a5b48dda5cf998bc3ab255
Paweł Bąkiewicz <pbakiewicz@icloud.com>
2024-12-29 17:08:54 +0100

    Dodanie wybieraka do taba.

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -1,12 +1,26 @@
 local cmp = require "cmp"
 
+-- tutaj dodać te automatyczne formatowanie
+
+
+-- Bez tego to taba zwykłego  nie użyję
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+-- local feedkey = function(key, mode)
+--  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+-- end
+
 cmp.setup {
     mapping = {
             ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+--      elseif vim.fn["vsnip#available"](1) == 1 then
+--        feedkey("<Plug>(vsnip-expand-or-jump)", "")
       elseif has_words_before() then
         cmp.complete()
       else
@@ -17,8 +31,8 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
+ --     elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+ --       feedkey("<Plug>(vsnip-jump-prev)", "")
       end
     end, { "i", "s" }),
         -- bez tego wsadzanie emojisów nie działa.
