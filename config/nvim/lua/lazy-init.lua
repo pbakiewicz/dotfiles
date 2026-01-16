9eb9b3620c0d4b652a4c98c06e8d7411947942ea
Paweł Bąkiewicz <pbakiewicz@icloud.com>
2026-01-16 18:37:12 +0100

    [Nvim] Add dashboard-nvim
    Skasowalem fold level bo mi dashboarfd-nvim przycinalo. I tak z tego nie
    korzystalem.

--- config/nvim/lua/lazy-init.lua
+++ config/nvim/lua/lazy-init.lua
@@ -14,7 +14,24 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-    "mhinz/vim-startify",
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup({
+                theme = 'hyper',
+                config = {
+                    week_header = { enable = true },
+                    shortcut = {
+                        { desc = "󰈞 Find", action = "Telescope find_files", key = "ff" },
+                        { desc = " Dotfiles", action = "lcd ~/o/dotfiles | Telescope find_files", key = "d" },
+                    },
+                }
+            })
+        end,
+        dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+    },
+
 
     {
         "f-person/git-blame.nvim",
